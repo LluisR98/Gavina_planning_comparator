c1c0ddccda308cbf37b7920d18a04251faea8340
LluisR98 <lluisrovira48@protonmail.com>
2021-05-30 21:25:26 +0200

    🆕 Fix: Rewrite the code to include scenarios and dates

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Little code to cross the planning Excels of "Gavina WiFi" and "Gavina WAN" proje
 
 ## Code execution:
 ```
-> planning-analyser.py --h
+> planning-analyser.py -h
 Usage: planning-analyser.py [-h] -wan WAN -scenario1 SCENARIO1 -scenario2 SCENARIO2
 
 Tool to cross-check planning schedules of Gavina WAN and Gavina WiFI projects.
@@ -20,18 +20,11 @@ Tool to cross-check planning schedules of Gavina WAN and Gavina WiFI projects.
 optional arguments:
   -h, --help            show this help message and exit
 
-Required named arguments:
-  -wan WAN              CSV WAN planning. Please put the WAN Admin in column number 3 (starting from 0).
-  -scenario1 SCENARIO1  CSV WiFi planning scenario 1. Please put the WAN Admin in column number 6 (starting from 0).
-  -scenario2 SCENARIO2  CSV WiFi planning scenario 2. Please put the WAN Admin in column number 3 (starting from 0).
+Required arguments:
+  -wan WAN              CSV WAN planning.
+  -scenario1 SCENARIO1  CSV WiFi planning scenario 1.
+  -scenario2 SCENARIO2  CSV WiFi planning scenario 2.
 ```
-## Preprocessing necessary to leave WAN administrative at:
-Start counting from 0.
-- File WAN Planning at column: 3.
-- File WiFi Scenario 1 at column: 6.
-- File WiFi Scenario 2 at column: 3.
 
 ## TODO:
-- [ ] Replace column number to field name when importing data.
-- [ ] Specify type of scenario.
-- [ ] Specify date of installation if available.
+- [ ] Nothing?
--- planning-analyser.py
+++ planning-analyser.py
@@ -7,105 +7,163 @@
 # Code execution:
 #   - $ planning-analyser.py --wan [WAN_planning.csv] --scenario1 [Scenario_1_planning.csv] --scenario2 [Scenario_2_planning.csv]
 #
-# Preprocessing necessary to leave administrative at:
-# Start counting from 0.
-#   - File WAN Planning at column: 3.
-#   - File WiFi Scenario 1 at column: 6.
-#   - File WiFi Scenario 2 at column: 3.
-#
 
 # Import time
 import argparse
-import os
+import csv
 import sys
-from csv import reader
-from csv import writer
+import os
 
 # Parse arguments 
 def get_args():
     parser = argparse.ArgumentParser(description="Tool to cross-check planning schedules of Gavina WAN and Gavina WiFI projects.")
     requiredvalues = parser.add_argument_group('Required arguments')
-    requiredvalues.add_argument('-wan', required=True, help='CSV WAN planning. Please put the WAN Admin in column number 3 (starting from 0).')
-    requiredvalues.add_argument('-scenario1', required=True, help='CSV WiFi planning scenario 1. Please put the WAN Admin in column number 6 (starting from 0).')
-    requiredvalues.add_argument('-scenario2', required=True, help='CSV WiFi planning scenario 2. Please put the WAN Admin in column number 3 (starting from 0).')
+    requiredvalues.add_argument('-wan', required=True, help='CSV WAN planning.')
+    requiredvalues.add_argument('-scenario1', required=True, help='CSV WiFi planning scenario 1.')
+    requiredvalues.add_argument('-scenario2', required=True, help='CSV WiFi planning scenario 2.')
     args = parser.parse_args()
     return args
 
 # Main menu
 if __name__ == "__main__":
-
     args = get_args()
-    scenario1_list = []
-    scenario2_list = []
-    wan_list = []
-    wifi_transformed = []
-    wifi_transformed_analysed = "WiFi_Transformed.csv"
+
+    wan_data = []
+    wan_school_data = []
+    wan_admin = []
+
+    scenario1_data = []
+    scenario1_school_data = []
+    scenario1_admin = []
+    scenario1_transformed = []
+
+    scenario2_data = []
+    scenario2_school_data = []
+    scenario2_admin = []
+    scenario2_transformed = []
+
+    admin_scenario2_match = []
+    admin_scenario1_match = []
+    wifi_transformed_filename = "WAN-WiFi_Analysed.csv"
+    csv_header = ['WAN','School Code','School Name','Type of scenario','Date']
 
 # Check if all parameters contain something
     if args.scenario1 != "none" and args.scenario2 != "none" and args.wan != "none":
 
 # Check if files exist
         if os.path.isfile(args.scenario1) and os.path.isfile(args.scenario2) and os.path.isfile(args.wan):
+            print()
 
-# Open CSV files and import WAN numbers
+# CSV WAN
             try:
-                with open(args.scenario1, 'r', encoding="utf8") as scenario1:
-                    scenario1_planning = reader(scenario1, delimiter=';')
-                    for school in scenario1_planning:
-                        scenario1_list.append(school[6])
+                with open(args.wan, 'r', encoding='UTF-8') as wan_planning:
+                    wan = csv.reader(wan_planning, delimiter=';')
+                    lines = 0
+                    for row in wan:
+                        if lines == 0:
+                            wan_columns = (row)
+                            lines += 1
+                        else:
+                            wan_data.append(row)
+                            lines += 1
+                    wan_index = wan_columns.index("ADMINISTRATIVO")
+                    wan_cod_centro = wan_columns.index("COD_CENTRO")
+                    wan_nom_centro = wan_columns.index("NOM_SEDE")
+                for wan_school in wan_data:
+                    wan_school_data.append(wan_school[wan_index] + ';' + wan_school[wan_cod_centro] + ';' + wan_school[wan_nom_centro])
+                    wan_admin.append(wan_school[wan_index])
             except:
-                print("ERROR: An error occurred while processing the file: " + args.scenario1)
+                print("Error while processing file: " + args.wan + ".")
+                sys.exit()
 
+# CSV Scenario 1
             try:
-                with open(args.scenario2, 'r', encoding="utf8") as scenario2:
-                    scenario2_planning = reader(scenario2, delimiter=';')
-                    for school in scenario2_planning:
-                        scenario2_list.append(school[3])
+                with open(args.scenario1, 'r', encoding='UTF-8') as scenario1_planning:
+                    scenario1 = csv.reader(scenario1_planning, delimiter=';')
+                    lines = 0
+                    for row in scenario1:
+                        if lines == 0:
+                            scenario1_columns = (row)
+                            lines += 1
+                        else:
+                            scenario1_data.append(row)
+                            lines += 1
+                    scenario1_index = scenario1_columns.index("Propietario conectividad")
+                    scenario1_cod_centro = scenario1_columns.index("Codi")
+                    scenario1_nom_centro = scenario1_columns.index("Centre")
+                for scenario1_school in scenario1_data:
+                    scenario1_school_data.append(scenario1_school[scenario1_index] + ';' + scenario1_school[scenario1_cod_centro] + ';' + scenario1_school[scenario1_nom_centro] + ';Scenario 1')
+                    scenario1_admin.append(scenario1_school[scenario1_index])
             except:
-                print("ERROR: An error occurred while processing the file: " + args.scenario2)
+                print("Error while processing file: " + args.scenario1 + ".")
+                sys.exit()
 
+# CSV Scenario 2
             try:
-                with open(args.wan, 'r', encoding="utf8") as wan:
-                    wan_planning = reader(wan, delimiter=';')
-                    for school in wan_planning:
-                        wan_list.append(school[3])
+                with open(args.scenario2, 'r', encoding='UTF-8') as scenario2_planning:
+                    scenario2 = csv.reader(scenario2_planning, delimiter=';')
+                    lines = 0
+                    for row in scenario2:
+                        if lines == 0:
+                            scenario2_columns = (row)
+                            lines += 1
+                        else:
+                            scenario2_data.append(row)
+                            lines += 1
+                    scenario2_index = scenario2_columns.index("WAN")
+                    scenario2_cod_centro = scenario2_columns.index("Codi")
+                    scenario2_nom_centro = scenario2_columns.index("Centre")
+                    scenario2_install = scenario2_columns.index("Data")
+                for scenario2_school in scenario2_data:
+                    scenario2_school_data.append(scenario2_school[scenario2_index] + ';' + scenario2_school[scenario1_cod_centro] + ';' + scenario2_school[scenario2_nom_centro] + ';Scenario 2;' + scenario2_school[scenario2_install])
+                    scenario2_admin.append(scenario2_school[scenario2_index])
             except:
-                print("ERROR: An error occurred while processing the file: " + args.wan)
+                print("Error while processing file: " + args.scenario2 + ".")
+                sys.exit()
 
-# Check the existence of the WAN numbers
-            for admin_wan in wan_list:
-                if admin_wan in scenario2_list and admin_wan not in wifi_transformed:
-                    wifi_transformed.append(admin_wan)
-                if admin_wan in scenario1_list and admin_wan not in wifi_transformed:
-                    wifi_transformed.append(admin_wan)
+# Cross-data time and avoid duplicates
+            for admin in wan_admin:
+                if admin in scenario2_admin:
+                    admin_scenario2_match.append(admin)
+                if admin in scenario1_admin and admin not in scenario2_admin:
+                    admin_scenario1_match.append(admin)
 
-# Check the results and generate a CSV with the results 
-            if len(wifi_transformed) != 0:
+# Check if file exists
+            if os.path.isfile(wifi_transformed_filename):
+                ask = input("WARNING!\n\nThis file already exists!\n\nDo you want to delete it and create a new one with the current version? [y/N] ")
+                if ask == "Y" or ask == "y":
+                    os.remove(wifi_transformed_filename)
+                else:
+                    print("No file has been modified.")
+                    sys.exit()
 
-# Check if the analysed file exists and delete it if necessary
-                if os.path.isfile(wifi_transformed_analysed):
-                    ask = input("WARNING!\n\nThis file already exists!\n\nDo you want to delete it and create a new one with the current version? [Y / N] ")
-                    if ask == "Y" or ask == "y":
-                        os.remove(wifi_transformed_analysed)
-                    elif ask == "N" or ask == "n":
-                        print("No file has been modified.")
-                        sys.exit()
-                    else:
-                        print("The requested character [Y] has not been detected. No file has been modified.")
-                        sys.exit()
+# Create the final CSV
+            try:
+                myData = [csv_header]
+                result = open(wifi_transformed_filename, 'w')
+                with result:
+                    writer = csv.writer(result)
 
-                try:
-                    with open(wifi_transformed_analysed, 'w', encoding="utf8", newline='') as output:
-                        result = writer(output)
-                        result.writerow(['WAN','Type of scenario','Date'])
-                        for school in wifi_transformed:
-                            result.writerow([school])
-                        print("Success!")
-                except:
-                    print("ERROR: An error occurred while processing the file: " + wifi_transformed_analysed)
+# Add Scenario 1 matches
+                    for school1 in admin_scenario1_match:
+                        for i in scenario1_school_data:
+                            temp = i.split(";")
+                            if temp[0] == school1:
+                                myData.append(temp)
+                    writer.writerows(myData)
 
-            else:
-                print("No matches found!")
+# Add Scenario 2 matches
+                    for school2 in admin_scenario2_match:
+                        for i in scenario2_school_data:
+                            temp = i.split(";")
+                            if temp[0] == school2:
+                                myData.append(temp)
+                    writer.writerows(myData)
+
+                    print("Success!")
+            except:
+                print("Error while processing the file: " + wifi_transformed_filename + ".")
+                sys.exit()
 
         else:
             print("Some of the requested files do not exist, please check it.")
